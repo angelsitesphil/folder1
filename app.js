c74cdc2e8e4b6d6139872cf993f4f8c72d2f6b46
angelsitesphil <markangelsantos724@gmail.com>
2025-11-05 23:09:02 +0800

    Simple Node PoC

---
+++ app.js
@@ -0,0 +1,1 @@
+const http = require("http"); const port = 3000; http .createServer((req, res) => { res.end("Hello from Simple DevSecOps PoC!"); }) .listen(port, () => console.log("Server running on port " + port));
